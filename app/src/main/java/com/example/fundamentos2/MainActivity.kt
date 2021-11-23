104029b9cc957d08d5eefed6b8bbbc6b3b3a426e
Jorge Gonzalez <jorgegonzalez97d@gmail.com>
2021-11-23 13:42:19 +0100

    Usando state en jetpack compose

---
+++ app/src/main/java/com/example/fundamentos2/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.fundamentos2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.fundamentos2.ui.theme.Fundamentos2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Fundamentos2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    HelloScreen()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun HelloContent(name: String, onNameChange: (String) -> Unit) {
+    //usando remember cuando gire la pantalla se inicializa todo desde el principio
+    //para evitar que se reinicie al girar usar rememberSaveable
+    //Para facilitar los test vamos a usar state hosting que es crear otra funcion que guarde el state
+    //en nuestro caso será HelloScreen
+    //var name: String by remember { mutableStateOf("")}
+    Column(
+        modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (!name.isEmpty()) {
+            Text("Hola mundo $name")
+        }
+        OutlinedTextField(
+            value = name,
+            onValueChange = onNameChange,
+            label = { Text("Nombre") }
+        )
+    }
+
+}
+
+@Composable
+fun HelloScreen() {
+    var name: String by rememberSaveable { mutableStateOf("") }
+    HelloContent(name = name, onNameChange = { name = it })
+}
+
+class HelloViewModel: ViewModel(){
+    private val _name: MutableLiveData<String> = MutableLiveData("")
+    val name: LiveData<String> get() = _name
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Fundamentos2Theme {
+        HelloScreen()
+    }
+}
